485c012e2495fa170d0f4fe002ed47766afc1cbf
Pedro Castaneda <petecast76@gmail.com>
2022-10-25 12:18:53 -0700

    First Commit Add 2 Numbers EP

---
+++ Controllers/AddTwoNumbersController.cs
@@ -0,0 +1,35 @@
+//Pedro Castaneda
+//10-25-2022
+//Add 2 Numbers - Endpoint
+//Ask user for two numbers then function adds them together
+//Peer Review by; 
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace castanedaPAddTwoNumbersEndpoint.Controllers
+{
+    [Route("[controller]")]
+    public class AddTwoNumbersController : Controller
+    {
+        [HttpGet]
+        [Route("Adding/{num1}/{num2}")]
+        public string Math(string num1, string num2)
+        {
+            int validNum1 = 0;
+            int validNum2 = 0;
+            bool checkNum1 = Int32.TryParse(num1, out validNum1);
+            bool checkNum2 = Int32.TryParse(num2, out validNum2);
+            string result = $"{validNum1} + {validNum2} = {validNum1 + validNum2}";
+            if(!checkNum1 || !checkNum2)
+            {
+                result = "Invalid Entry";
+            }
+            return result;
+        }
+    }        
+}
